ccd08dcfd4a9a24ce9cb3c1ab711790cad3bd023
Mariia Tokhtamysh <mariia.tokhtamysh@nure.ua>
2025-05-29 18:06:17 +0300

    Create index.php

---
+++ Lab3/smp-pzpi-23-5-tokhtamysh-mariia-lab3/index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Spring Store</title>
+  
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+  <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700&display=swap" rel="stylesheet">
+  
+  <link rel="stylesheet" href="styles.css" />
+</head>
+<body>
+  <nav class="navbar">
+    <a href="#" class="nav-item">
+      <i class="fa-solid fa-house"></i>
+      <span>Home</span>
+    </a>
+    <div class="separator"></div>
+    <a href="pages/product.php" class="nav-item">
+      <i class="fa-solid fa-rectangle-list"></i>
+      <span>Products</span>
+    </a>
+    <div class="separator"></div>
+    <a href="pages/basket.php" class="nav-item">
+      <i class="fa-solid fa-cart-shopping"></i>
+      <span>Cart</span>
+    </a>
+  </nav>
+  <main class="home-page">
+  <h1>🌿 Welcome to Spring Store – Nature in Every Detail</h1>
+  <p>Discover eco-friendly products for your home, garden, and lifestyle.</p>
+
+  <div class="home-highlights">
+    <div class="highlight">
+      <i class="fa-solid fa-leaf"></i>
+      <p>Eco-friendly Selection</p>
+    </div>
+    <div class="highlight">
+      <i class="fa-solid fa-truck-fast"></i>
+      <p>Fast Delivery in Ukraine</p>
+    </div>
+    <div class="highlight">
+      <i class="fa-solid fa-hand-holding-heart"></i>
+      <p>Support Local Producers</p>
+    </div>
+  </div>
+
+  <div class="home-page__buttons">
+    <a href="pages/product.php" class="btn">Start Shopping</a>
+  </div>
+</main>
+
+    <footer class="footer">
+      <div class="footer__nav">
+        <a href="index.php" class="footer__link">Home</a> |
+        <a href="pages/product.php" class="footer__link">Products</a> |
+        <a href="pages/basket.php" class="footer__link">Cart</a> |
+        <a href="pages/about_us.php" class="footer__link">About us</a> 
+      </div>
+    </footer>
+</body>
+</html>
