874776cdf42eea2fa9163d1c832fdf3fbcd1c1ed
Mariia Tokhtamysh <mariia.tokhtamysh@nure.ua>
2025-06-03 19:45:11 +0300

    Update profile.php

--- Lab4/smp-pzpi-23-5-tokhtamysh-mariia-lab4/db/profile.php
+++ Lab4/smp-pzpi-23-5-tokhtamysh-mariia-lab4/db/profile.php
@@ -1,9 +1,9 @@
 <?php
-$profile = array (
+$userProfile = array (
   'first_name' => 'Mariiia1111',
   'last_name' => 'Tokhtamysh',
   'birth_date' => '2006-08-26',
-  'bio' => 'My name is Mariia. I am studing Sowtware Engenering',
-  'profile_picture' => 'img_683ecec8bce195.44404998.jpg',
+  'bio' => 'My name is Mariia. I am studying Software Engineering',
+  'profile_picture' => 'photo_6d1c5717a000490a3299984fc4589855683f1f05a79760.18541462.jpg',
 );
 ?>
